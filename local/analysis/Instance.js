b0d83c09f4d3847901523a265f175f84f676657e
Ryan <ryan.haynes@gmail.com>
2017-09-11 02:23:10 -0400

    Added pulling of remote model and source files and creation of analysis directory

--- local/analysis/Instance.js
+++ local/analysis/Instance.js
@@ -1,12 +1,13 @@
 
 var fs = require('fusion/fs');
 var path = require('path');
+var sleep = require('fusion/sleep');
 var Shell = require('fusion/Shell');
 
 function Instance(node,id) {
   this.node = node;
   this.id = id;
-  this.analysisDir = path.join(Const.workspaceDir,node,id);
+  this.analysisDir = path.join(Const.workspaceDir,node,''+id);
 }
 
 Instance.prototype.file = function(name) {
@@ -21,20 +22,36 @@ Instance.prototype.rm = function(name) {
   if (fs.exists(this.file(name))) fs.unlink(this.file(name));
 }
 
-Instance.prototype.rmdir = function(name) {
-  if (fs.exists(this.file(name))) {
-    var sh = new Shell();
-    sh.run('rm -rf ?',this.file(name));
-  }
-}
-
-Instance.prototype.run = function(id) {
+Instance.prototype.run = function() {
   if (!this.inProgress()) {
     this.rm('Done');
     this.rm('Summary.txt');
-    this.rmdir('NONMEM.g77')
-//    fs.writeFile(path.join(this.analysisDir,'Ready'),'');
+    fs.writeFile(path.join(this.analysisDir,'Ready'),'');
   }
+}
+
+Instance.prototype.waitForResults = function() {
+  while(true) {
+    if (fs.exists(this.file('Done'))) return;
+    sleep(500);
+  }
+}
+
+function convert2obj(lines) {
+  var obj = {};
+  for (var k in lines) {
+    var parts = lines[k].split('=');
+    obj[parts[0]] = parts[1];
+  }
+  return obj;
+}
+
+Instance.prototype.pushResults = function() {
+  var summary = convert2obj(fs.readFile(this.file('Summary.txt'),'utf8').split('\r\n'));
+  http.post(Const.analysisServer+'submitJob',{
+    id: this.id,
+    data: summary,
+  });
 }
 
 module.exports = Instance;
--- routes/run/startAnalysis.js
+++ routes/run/startAnalysis.js
@@ -1,20 +1,22 @@
 
 var http = require('fusion/http');
 var AnalysisInstance = require('analysis/Instance');
-
 var fs = require('fusion/fs');
+var csv = require('fusion/csv');
 var path = require('path');
 var Shell = require('fusion/Shell');
 
-var modelFiles = ['Fields.txt','%.csv','%.fit.txt','%_0.ctl','%_1.ctl'];
+var modelFiles = ['Fields.txt','%.csv','%.fit.txt','%_0.ctl','%_1.ctl','%.base.txt'];
 
-function cloneDrugModel(analysis) {
+function cloneDrugModel(nodeName,analysis) {
   var model = analysis.model;
   var drugModelDir = path.join(Const.modelDir,model.key);
+  var shell = new Shell();
+
+  // If the drug model directory doesn't exist, create it
   if (!fs.exists(drugModelDir)) {
     fs.mkdir(drugModelDir);
 
-    var shell = new Shell();
     shell.cd(drugModelDir);
     for (var k in modelFiles) {
       var file = modelFiles[k].replace(/\%/g,model.name);
@@ -22,22 +24,75 @@ function cloneDrugModel(analysis) {
       shell.run('curl --compressed ? > '+file,[url]);
     }
   }
+
+  // Copy files from the drug model directory to the directory for analysis
+  var analysisDir = path.join(Const.workspaceDir,nodeName,''+analysis.job.jobID);
+  if (!fs.exists(analysisDir)) fs.mkdir(analysisDir);
+  shell.cd(analysisDir);
+
+  function cp(from,to) {
+    var file = from.replace(/\%/g,model.name);
+    shell.run('cp ? ?',[path.join(drugModelDir,file),to.replace(/\%/g,model.name)]);
+  }
+  cp('%.fit.txt','fit.key.txt');
+  cp('%_1.ctl','NONMEM.ctl');
+  cp('%_1.ctl','NONMEM.ctl');
+  cp('%.csv','%.csv');
+  cp('%.base.txt','BaseObjFn.txt');
+
+  // Copy Analyze.bat
+  shell.run('cp ? .',[path.join(Const.modelDir,'Analyze.bat')]);
+
+  // Modify the csv file
+  var outputCSV = '';
+  var rows = csv.csv2json(path.join(analysisDir,model.name+'.csv'));
+  rows[0].push('A');
+
+  var idx = 1;
+  var id2idx = {};
+  for (var k in analysis.regressor) {
+    id2idx[k] = idx;
+    idx++;
+  }
+  outputCSV += rows[0].join(',')+'\r\n';
+  for (var k=1;k<rows.length;k++) {
+    var id = rows[k][0];
+    rows[k][0] = id2idx[id];
+    rows[k].push(analysis.regressor[id]);
+    outputCSV += rows[k].join(',')+'\r\n';
+  }
+  fs.writeFile(path.join(analysisDir,model.name+'.csv'),outputCSV);
 }
 
-function startAnalysis(analysis) {
+function startAnalysis(nodeName,analysis) {
 
-  cloneDrugModel(analysis);
+  // Get drug model for the analysis
+  cloneDrugModel(nodeName,analysis);
 
-  //var instance = new AnalysisInstance(name,'792429');
-  //instance.run();
+  var instance = new AnalysisInstance(nodeName,analysis.job.jobID);
+  instance.run();
+
+  instance.waitForResults();
+  instance.pushResults();
+  console.log('Done with analysis');
 }
+
 
 exports.main = function() {
+/*  var res = { model:
+   { model: 1,
+     name: 'Olanzapine',
+     key: 'HgkfvrZiQGuNaqMA58_xquq8RbimUECr' },
+  job: { jobID: 9, name: 'gene8', model: 1 },
+  regressors: [ [] ],
+  success: 1 };
+*/
   var res = JSON.parse(http.post(Const.analysisServer+'nextJob',{
     ip: '127.0.0.1',
   }));
 
-  if (res) startAnalysis(res);
+  if (res && res.model) startAnalysis('Analysis_Node1',res)
+  else console.log('No available jobs.');
 
 //  startAnalysis('Analysis_Node1');
 //  startAnalysis('Analysis_Node2');
