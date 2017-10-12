82cdbec138f83401bc04c3f5234e907b1930fc04
Ryan <ryan.haynes@gmail.com>
2017-10-12 14:36:17 -0400

    Added ability to start multiple virtual machines

--- routes/cron/checkVM.js
+++ routes/cron/checkVM.js
@@ -1,23 +1,12 @@
 
-var Shell = require('fusion/Shell');
+var vm = require('analysis/vm');
 
 // This sets how often checkJobs runs
 exports.schedule = '0 * * * * *';
 
-function isRunningVM() {
-  var shell = new Shell();
-  var list = shell.run('vboxmanage list runningvms',[]).split('\n');
-  for (var k in list)
-    if (list[k]) return true;
-  return false;
-}
-
 exports.main = function($C) {
-  console.log('check VM');
 
-  if (!isRunningVM()) {
-    var shell = new Shell();
-    shell.run('vboxmanage startvm WinXP --type headless',[]);
-  }
+  console.log('check VM');
+  vm.startAll();
 }
 
