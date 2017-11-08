aec8f2a3b318b0f4a696a001fd492795431c31e3
Ryan <ryan.haynes@gmail.com>
2017-11-08 01:04:03 -0500

    Fixed typo in analysis/watch

--- routes/run/setCloudWatchAlarm.js
+++ routes/run/setCloudWatchAlarm.js
@@ -4,8 +4,8 @@ var sleep = require('fusion/sleep');
 
 exports.main = function() {
   if (Const.wantsCloudWatch) {
-    // Sleep for 1 minute before setting the alarm to allow for startup
-    sleep(60000);
+    // Sleep for 5 minutes before setting the alarm to allow for startup
+    sleep(60000*5);
 
     watch.set();
   }
