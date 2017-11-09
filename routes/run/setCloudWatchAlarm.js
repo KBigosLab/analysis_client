316db65bae4f88b0188cafcb1b91da6aed804acd
Ryan <ryan.haynes@gmail.com>
2017-11-08 23:37:28 -0500

    Modified alarm startup timing

--- routes/run/setCloudWatchAlarm.js
+++ routes/run/setCloudWatchAlarm.js
@@ -4,11 +4,11 @@ var sleep = require('fusion/sleep');
 
 exports.main = function() {
   if (Const.wantsCloudWatch) {
-    // Sleep for 15 minutes before setting the alarm to allow for startup
-    sleep(60000*15);
+    // Sleep for 5 minutes before setting the alarm to allow for startup
+    sleep(60000*5);
 
     watch.set();
-    sleep(5000);
+    sleep(60000*10);
     watch.reset();
   }
 }
